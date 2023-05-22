e4a080d7813711a136a77eff842896b9edb95c56
Alexandre Damas Murata <125297509+AlexandreDSptech@users.noreply.github.com>
2023-05-22 14:12:06 -0300

    Update usuarioModel.js

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -13,7 +13,7 @@ function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
     SELECT CASE 
-        WHEN fun_valida_usuario('${email}','${senha}') = 1 
+        WHEN fun_valida_usuario('${email}','sha2(${senha},256)') = 1 
         THEN 'Login realizado com sucesso'
         ELSE 'Login ou senha incorreta' END Validação;
     `;
